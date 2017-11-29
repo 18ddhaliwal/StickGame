2921d437a1bd8cce452fd05d8c5e82e672038d83
18zm <34075821+18zm@users.noreply.github.com>
2017-11-29 11:41:24 -0800

    Second Commit

--- src/StickGame.java
+++ src/StickGame.java
@@ -1,12 +1,20 @@
 public class StickGame {
-    private int num;//number of sticks
-    private static StickPlayer[]
+    private int numRounds;
+    private static StickPlayer[] players;
     public StickBoard(int board){
-        this.num = num;
+        this.players = players;
+        numRounds = 1000;
     }
+    public void round(){
+        for (int i = 0; i < players.length; )
+    }
+
+
+
+
     public int getNum(){
-        return num;
+        return
     }
-    public int takeSticks
+
 
 }
