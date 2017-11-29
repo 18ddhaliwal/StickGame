57ef5ea13e3e48ebef9f161e48baa7e7ca0cd09f
18zm <34075821+18zm@users.noreply.github.com>
2017-11-29 11:06:59 -0800

    Second Commit

---
+++ src/Player1.java
@@ -0,0 +1,3 @@
+public class StickPlayer extends StickGame{
+
+}
--- src/StickGame.java
+++ src/StickGame.java
@@ -1,3 +1,12 @@
 public class StickGame {
+    private int num;//number of sticks
+    private static StickPlayer[]
+    public StickBoard(int board){
+        this.num = num;
+    }
+    public int getNum(){
+        return num;
+    }
+    public int takeSticks
 
 }
